bf901a84a0d88d7320c2010754c8b063222b4774
cho-masa <xxx@xxx.co.jp>
2016-01-30 01:06:29 +0900

    ロケールはクライアントで保持するように変更

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+//import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 // 起動クラス
 @SpringBootApplication
@@ -17,13 +18,23 @@ public class Application extends WebMvcAutoConfigurationAdapter{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	// ロケールをサーバ（セッション）で保持する場合
+	//@Bean
+	//public SessionLocaleResolver localeResolver) {
+	//  SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	//  localeResolver.setDefaultLocale(Locale.JAPAN);
+	//  return localeResolver;
+	//}
+
+    // ロケールをクライアント（Cookie）で保持する場合
+    @Bean 
+    public CookieLocaleResolver localeResolver() { 
+            CookieLocaleResolver localeResolver = new CookieLocaleResolver(); 
+            localeResolver.setDefaultLocale(Locale.JAPAN); 
+            return localeResolver; 
+    }
 	
-	@Bean
-	public SessionLocaleResolver localeResolver() {
-	  SessionLocaleResolver r = new SessionLocaleResolver();
-	  r.setDefaultLocale(Locale.JAPAN);
-	  return r;
-	}
 	
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
